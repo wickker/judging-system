fe0c7ab895f312f70f51f023f801577ed110266b
choydianchun@gmail.com <choydianchun@gmail.com>
2023-06-12 02:57:36 +0800

    add count relation to query, edit icon

--- src/lib/dtos/competitions.ts
+++ src/lib/dtos/competitions.ts
@@ -14,6 +14,21 @@ const useCompetitionDto = (db: PrismaClient) => {
 	const findAllByUser = async (userId: number) =>
 		await db.competition.findMany({
 			where: { userId, deletedAt: null },
+			select: {
+				id: true,
+				name: true,
+				year: true,
+				_count: {
+					select: {
+						sessions: {
+							where: { deletedAt: null },
+						},
+					},
+				},
+			},
+			orderBy: {
+				createdAt: 'desc', // newest first
+			},
 		})
 
 	return { create, findAllByUser }
--- src/routes/competitions/+page.svelte
+++ src/routes/competitions/+page.svelte
@@ -1,12 +1,14 @@
 <script lang="ts">
 	import { Header, ButtonCircularPlus } from '$lib/components/commons'
 	import { AddCompetitionModal } from '$lib/components/Competitions'
+	import IconEdit from '$lib/assets/icon-edit-zinc.svg'
 
 	export let data
 
 	let isAddCompetitionModalVisible = false
 
 	$: competitions = data.competitions
+	$: console.log(competitions)
 
 	function handleOpenAddCompeitionModal() {
 		isAddCompetitionModalVisible = true
@@ -20,13 +22,13 @@
 </Header>
 
 <div class="px-3 py-4">
-	{#each competitions as { id, name, year } (id)}
+	{#each competitions as { id, name, year, _count } (id)}
 		<div class='py-2'>
 			<div class="flex items-center justify-between text-lg">
 				{name} ({year})
-				<div>icon</div>
+				<img src={IconEdit} alt="Icon edit" class="h-6 w-6" />
 			</div>
-			<p class="text-base text-zinc-500">x sessions</p>
+			<p class="text-base text-zinc-500">{_count.sessions} sessions</p>
 		</div>
 	{/each}
 </div>
