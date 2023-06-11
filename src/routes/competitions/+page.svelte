409c3aa98d5e07b41aed8b07ebf056750565e19f
choydianchun@gmail.com <choydianchun@gmail.com>
2023-06-12 02:35:07 +0800

    competition tile skeleton

--- src/routes/competitions/+page.svelte
+++ src/routes/competitions/+page.svelte
@@ -4,9 +4,9 @@
 
 	export let data
 
-	$: console.log('data : ', data)
-
 	let isAddCompetitionModalVisible = false
+
+	$: competitions = data.competitions
 
 	function handleOpenAddCompeitionModal() {
 		isAddCompetitionModalVisible = true
@@ -18,6 +18,18 @@
 		<ButtonCircularPlus on:click={handleOpenAddCompeitionModal} />
 	</div>
 </Header>
+
+<div class="px-3 py-4">
+	{#each competitions as { id, name, year } (id)}
+		<div class='py-2'>
+			<div class="flex items-center justify-between text-lg">
+				{name} ({year})
+				<div>icon</div>
+			</div>
+			<p class="text-base text-zinc-500">x sessions</p>
+		</div>
+	{/each}
+</div>
 
 {#if isAddCompetitionModalVisible}
 	<AddCompetitionModal bind:isVisible={isAddCompetitionModalVisible} />
